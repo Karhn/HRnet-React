07d698770ec373e4283a5fc411904c2879b0efde
Karhn <drkarhn@gmail.com>
2026-06-08 16:24:17 +0200

    Utilisation du package npm pour le tableau de la liste des employees

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -1,5 +1,20 @@
 import { Link } from "react-router-dom"
 import { useAppSelector } from "../hooks/reduxHooks"
+import { DataTable, type TableColumn } from "hrnet-react-tableau"
+import "hrnet-react-tableau/dist/style.css"
+import type { Employee } from "../types/employee"
+
+const columns: TableColumn<Employee>[] = [
+    { key: "firstName", label: "First Name" },
+    { key: "lastName", label: "Last Name" },
+    { key: "startDate", label: "Start Date" },
+    { key: "department", label: "Department" },
+    { key: "dateOfBirth", label: "Date of Birth" },
+    { key: "street", label: "Street" },
+    { key: "city", label: "City" },
+    { key: "state", label: "State" },
+    { key: "zipCode", label: "Zip Code" },
+]
 
 function EmployeeList() {
     const employees = useAppSelector((state) => state.employees.employees)
@@ -8,43 +23,7 @@ function EmployeeList() {
         <main className="container">
             <h1> Current Employees </h1>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th> First Name </th>
-                        <th> Last Name </th>
-                        <th> Start Date </th>
-                        <th> Department </th>
-                        <th> Date of Birth </th>
-                        <th> Street </th>
-                        <th> City </th>
-                        <th> State </th>
-                        <th> Zip Code </th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {employees.length === 0 ? (
-                        <tr>
-                            <td colSpan={9}> No employees yet </td>
-                        </tr>
-                    ) : (
-                        employees.map((employee, index) => (
-                            <tr key={`${employee.firstName}-${employee.lastName}-${index}`}>
-                                <td>{employee.firstName}</td>
-                                <td>{employee.lastName}</td>
-                                <td>{employee.startDate}</td>
-                                <td>{employee.department}</td>
-                                <td>{employee.dateOfBirth}</td>
-                                <td>{employee.street}</td>
-                                <td>{employee.city}</td>
-                                <td>{employee.state}</td>
-                                <td>{employee.zipCode}</td>
-                            </tr>
-                        ))
-                    )}
-                </tbody>
-            </table>
+            <DataTable columns={columns} data={employees}/>
 
             <Link to="/"> Home </Link>
         </main>
